f86efd1634c910ae1a51248cb638326c607d7791
Jawad Dhew <jawaddhew98@gmail.com>
2022-10-26 00:31:27 +0600

    course api

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ app.get('/courses', (req, res) => {
     res.send(courses)
 });
 
-app.get('/courses/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const course = courses.filter(n => n.id === id);
-    res.send(course);
 
-})
 
 app.listen(port, () => {
     console.log('Server running on port', port);
